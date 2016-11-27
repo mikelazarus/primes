f0cdff11faf815d44a8aec3e358c34e817bd0766
mikelazarus <mikelazarus@yahoo.com>
2016-11-26 23:04:54 -0800

    Testing Pycharm VCS

--- test_is_prime.py
+++ test_is_prime.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 
 class TestIs_prime(TestCase):
+    """Class for testing prime number utilities."""
     def test_is_prime(self):
         from check_prime import is_prime
         self.assertTrue(is_prime(5))
